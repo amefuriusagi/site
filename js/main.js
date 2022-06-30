7b6251127fc9ef12da571ffdda76751709a0bb46
amefuriusagi <108465327+amefuriusagi@users.noreply.github.com>
2022-06-30 21:02:33 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -41,3 +41,50 @@ function rese(){
 	}
 disp()
 }
+
+function twit() {
+	var s, url;
+	s = "12キャラ対決結果";
+	url = document.location.href;
+	
+	var dt = new Date();
+	var y = dt.getFullYear();
+	var m = ('00' + (dt.getMonth()+1)).slice(-2);
+	var d = ('00' + dt.getDate()).slice(-2);
+	var result = y + '-' + m + '-' + d;
+	s=s +"%0D%0A"+ result
+	s = s +"%0D%0A"+ document.getElementById('name1').value+"%0D%0A"
+	for(var i=1;i<13;i++){
+		var srcchk=document.getElementById('img1'+i).src.substr(-5,1)
+		if(srcchk=="u"){
+			s=s+"🗯"
+		}else{
+			s=s+"🎈"
+		}
+		if(i==6){
+		s=s+"%0D%0A"}
+	}
+	
+	s=s+"%0D%0A"+ document.getElementById('name2').value+"%0D%0A"
+	for(var i=1;i<13;i++){
+		var srcchk=document.getElementById('img2'+i).src.substr(-5,1)
+		if(srcchk=="u"){
+			s=s+"🗯"
+		}else{
+			s=s+"🎈"
+		}
+		if(i==6){
+		s=s+"%0D%0A"}
+	}	
+	
+	s = s +"%0D%0A"+"%0D%0A"
+	
+		if (s.length > 140) {
+			//文字数制限
+			alert("テキストが140字を超えています");
+		} else {
+			//投稿画面を開く
+			url = "http://twitter.com/share?url=" + escape(url) + "&text=" + s;
+			window.open(url,"_blank","width=600,height=300");
+		}
+}
